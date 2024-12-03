62cd09f3b6f03c011701b920298a99c62ca622a3
Jonathan Alveraldo Bangun <alveraldo.jonathan@gmail.com>
2024-12-03 16:46:45 +0700

    Add files via upload

---
+++ persegipanjang_1.py
@@ -0,0 +1,14 @@
+# JUDUL PROGRAM
+# Program Persegi Panjang
+
+# KAMUS
+# panjang  :  integer
+# lebar    :  integer
+# i        :  integer
+
+# ALGORTIMA
+panjang = int(input("Masukkan panjang persegi panjang: "))
+lebar = int(input("Masukkan lebar persegi panjang: "))
+
+for i in range(lebar):
+    print("*" * panjang)
