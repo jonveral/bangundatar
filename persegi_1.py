094b2fab57d839d7bbea97745f58a9b752a9a142
jonveral <alveraldo.jonathan@gmail.com>
2024-12-03 16:35:47 +0700

    Add files via upload

---
+++ persegi_1.py
@@ -0,0 +1,10 @@
+# JUDUL PROGRAM
+# Program Persegi
+
+# KAMUS
+# sisi  :  integer
+# i     :  integer
+sisi = int(input("Masukkan panjang sisi persegi: "))
+
+for i in range(sisi):
+    print("*" * sisi)
